6820e567eff5ec68c43af41492696e99f182d64b
samuelboada21 <samuelomarboba@ufps.edu.co>
2023-11-19 18:34:29 -0500

    LoadBalanced

--- Usuario-Service/src/main/java/com/usuario/service/configuration/RestTemplateConfig.java
+++ Usuario-Service/src/main/java/com/usuario/service/configuration/RestTemplateConfig.java
@@ -1,6 +1,7 @@
 
 package com.usuario.service.configuration;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
